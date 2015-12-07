33b6f2896b31a005918a89cfc1200759148eec36
n-tp <tamphubkdn@gmail.com>
2015-12-07 14:52:25 +0700

    [add] Destroy user by soft delete

--- db/migrate/20151204070746_create_users.rb
+++ db/migrate/20151204070746_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.integer :age
+      t.boolean :is_deleted, default: false
 
       t.timestamps null: false
     end
